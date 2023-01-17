e22eaaa223a71c8446367dec2b5b6648aa83b458
Roger Wells <rsw359@gmail.com>
2023-01-17 16:07:57 +0900

    login double render fixed

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
 
-//register
+//REGISTER
 router.post("/register", async (req, res) => {
   try {
     const salt = await bcrypt.genSalt(10);
@@ -20,14 +20,14 @@ router.post("/register", async (req, res) => {
   }
 });
 
-//login
+//LOGIN
 router.post("/login", async (req, res) => {
   try {
     const user = await User.findOne({ username: req.body.username });
-    !user && res.status(400).json("Wrong Credentials");
+    if (!user) return res.status(400).json("Wrong credentials!");
 
     const validated = await bcrypt.compare(req.body.password, user.password);
-    !validated && res.status(400).json("Wrong Credentials");
+    if (!validated) return res.status(400).json("Wrong credentials!");
 
     const { password, ...others } = user._doc;
     res.status(200).json(others);
